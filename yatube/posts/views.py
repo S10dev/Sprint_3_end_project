739c0522a12b8dfc627216e5ab5d7e4e21b3d4e2
Ioann <s10.dev@yandex.ru>
2021-02-18 09:28:14 +0300

    passed tests

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,15 @@
 from django.http import HttpResponse
-from .models import Post
-from django.shortcuts import render 
+from .models import Post, Group
+from django.shortcuts import render, get_object_or_404
 # Create your views here.
 
 
 def index(request):
-    latest = Post.objects.order_by('-pub_date')[:10]
+    latest = Post.objects[:10]
     return render(request, "index.html", {"posts": latest} )
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug = slug)
+    posts = Post.objects.filter(group=group)[:12]
+    return render(request, "group.html",{"group": group, "posts": posts})
