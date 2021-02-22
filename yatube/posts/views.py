470b2e203caef7344114d791c897adaa9f98652f
Ioann <s10.dev@yandex.ru>
2021-02-22 10:12:12 +0300

    added search

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,17 +1,24 @@
 from django.http import HttpResponse
 from .models import Post, Group
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import SearchForm
+import datetime as dt
 # Create your views here.
 
 
 def index(request):
-    keyword = request.GET.get("q", None)
-    if keyword:
-        posts = Post.objects.select_related("author").filter(text__contains=keyword)
-    else:
-        posts = None
-
-    return render(request,"index.html", {"posts": posts, "keyword": keyword})
+    if request.method =='POST':
+        req = request.POST['search'].replace(' ','+')
+        return redirect(f'/?q={req}')
+    elif request.method =='GET':
+        keyword = request.GET.get("q", None)
+        form = SearchForm
+        if keyword:
+            posts = Post.objects.select_related("author").filter(text__contains=keyword)
+        else:
+            posts = None
+         
+        return render(request,"index.html", {"posts": posts, "keyword": keyword, "form": form})
 
 
 
